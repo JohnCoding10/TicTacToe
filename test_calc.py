db07deeda3306ec7c189641046af249d6c14c975
SamJuckes <samuel.juckes@kyndryl.com>
2025-07-18 11:53:40 +0100

    Update test_calc.py by adding the test for divide and multiply

--- test_calc.py
+++ test_calc.py
@@ -10,3 +10,13 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(0, 0) == 0
+    assert divide(-1, -1) == 1
+
+def test_multiply():
+    assert multiply(5, 3) == 15
+    assert multiply(0, 0) == 0
+    assert multiply(-1, -1) == 1 
